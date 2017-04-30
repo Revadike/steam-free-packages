f4f11dc8989d1f8ab0016a677d4588f3a17cf2b4
Royalgamer06 <royalgamer06@hotmail.com>
2017-04-30 21:50:27 +0200

    Update install.bat

--- install.bat
+++ install.bat
@@ -1,11 +1,12 @@
 @ECHO OFF
 CD %~dp0
-npm install steamauth > install.log
-npm install steam-user >> install.log
-npm install steamcommunity >> install.log
-npm install jsdom@9.12.0 >> install.log
-npm install fs >> install.log
-npm install request >> install.log
-npm install cloudscraper >> install.log
-npm install jquery >> install.log
+npm install fs >>install.log
+npm install request >>install.log
+npm install cloudscraper >>install.log
+npm install jsdom@9.12.0 >>install.log
+npm install jquery >>install.log
+npm install node-steam >install.log
+npm install steamauth >>install.log
+npm install steam-user >>install.log
+npm install steamcommunity >>install.log
 pause
