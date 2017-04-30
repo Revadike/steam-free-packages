47d308fb11fd66609ab6cf1aeba04c889b8c0c64
Royalgamer06 <royalgamer06@hotmail.com>
2017-04-30 16:50:17 +0200

    Update install.bat

--- install.bat
+++ install.bat
@@ -2,8 +2,9 @@
 npm install steamauth
 npm install steam-user
 npm install steamcommunity
-npm install jsdom
+npm install jsdom@9.12.0
 npm install fs
 npm install request
 npm install cloudscraper
+npm install jquery
 pause
