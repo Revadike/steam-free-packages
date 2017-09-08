8c982a0b27164c964be666bd07dfe97d0ec9b053
Royalgamer06 <royalgamer06@hotmail.com>
2017-09-08 18:31:19 +0200

    Update install.bat

--- install.bat
+++ install.bat
@@ -1,11 +1,5 @@
 @ECHO OFF
-CD %~dp0
+CD /D %~dp0
 start npm install fs
-start npm install request
-start npm install cloudscraper
-start npm install jsdom@9.12.0
-start npm install jquery
-start npm install node-steam
 start npm install steamauth
 start npm install steam-user
-start npm install steamcommunity
