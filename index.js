abb4ee40187780029a498e2b0e365c7618ec34a1
Royalgamer06 <royalgamer06@hotmail.com>
2017-09-08 18:30:45 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -1,16 +1,13 @@
-var SteamAuth = require("steamauth"),
-SteamUser = require("steam-user"),
-SteamCommunity = require("steamcommunity"),
-jsdom = require("jsdom"),
-$ = require("jquery")(jsdom.jsdom().defaultView),
-fs = require("fs"),
-request = require("request"),
-CloudScraper = require("cloudscraper");
-
+const SteamUser = require("steam-user"),
+    SteamAuth = require("steamauth"),
+    util = require("util"),
+    fs = require("fs");
+var logStream = fs.createWriteStream("log.txt", { "flags": "a" });
 var log = console.log;
 console.log = function() {
     var first_parameter = arguments[0];
     var other_parameters = Array.prototype.slice.call(arguments, 1);
+
     function formatConsoleDate(date) {
         var day = date.getDate();
         var month = date.getMonth() + 1;
@@ -20,28 +17,51 @@ console.log = function() {
         var seconds = date.getSeconds();
         var milliseconds = date.getMilliseconds();
         return "[" + ((day < 10) ? "0" + day : day) +
-        "-" + ((month < 10) ? "0" + month : month) +
-        "-" + ((year < 10) ? "0" + year : year) +
-        " " + ((hour < 10) ? "0" + hour : hour) +
-        ":" + ((minutes < 10) ? "0" + minutes : minutes) +
-        ":" + ((seconds < 10) ? "0" + seconds : seconds) +
-        "." + ("00" + milliseconds).slice(-3) + "] ";
+            "-" + ((month < 10) ? "0" + month : month) +
+            "-" + ((year < 10) ? "0" + year : year) +
+            " " + ((hour < 10) ? "0" + hour : hour) +
+            ":" + ((minutes < 10) ? "0" + minutes : minutes) +
+            ":" + ((seconds < 10) ? "0" + seconds : seconds) +
+            "." + ("00" + milliseconds).slice(-3) + "] ";
     }
-    log.apply(console, [formatConsoleDate(new Date()) + first_parameter].concat(other_parameters));
-}
-
-var config = JSON.parse(fs.readFileSync("config.json"));
-var client = new SteamUser({
-        enablePicsCache: true
+    var tolog = [formatConsoleDate(new Date()), first_parameter].concat(other_parameters);
+    var str = "";
+    tolog.forEach(function(arg) {
+        str += (typeof arg === "string" ? arg : util.inspect(arg, false, null)) + " ";
     });
-var ownedSubs = [];
+    str.slice(0, -1);
+    logStream.write(str + "\r\n");
+    log.apply(console, [str]);
+};
+
+const config = JSON.parse(fs.readFileSync("config.json"));
+const client = new SteamUser({
+    enablePicsCache: true,
+    changelistUpdateInterval: 200,
+    picsCacheAll: true
+});
+
+var ownedPackages = [],
+    fodQueue = [],
+    fodRequested = 0,
+    mycountry = "US";
+
+setInterval(function() {
+    fodRequested = 0;
+    if (fodQueue.length > 0) {
+        for (var i = 0; i < fodQueue.length && i <= 50; i++) {
+            requestFreeSub(fodQueue.pop());
+        }
+    }
+}, 3600000);
+
 if (config.winauth_usage) {
+    const SteamAuth = require("steamauth");
     SteamAuth.Sync(function(error) {
-        if (error)
-            console.log(error);
+        if (error) console.log(error);
         var auth = new SteamAuth(config.winauth_data);
         auth.once("ready", function() {
-            config.steam_credentials.authCode = config.steam_credentials.twoFactorCode = auth.calculateCode();
+            config.steam_credentials.twoFactorCode = auth.calculateCode();
             steamLogin();
         });
     });
@@ -51,181 +71,95 @@ if (config.winauth_usage) {
 
 function steamLogin() {
     config.steam_credentials.rememberPassword = true;
+    config.steam_credentials.logonID = Date.now();
     client.logOn(config.steam_credentials);
     client.on("loggedOn", function(response) {
         console.log("Logged into Steam as " + client.steamID.getSteam3RenderedID());
-        client.setPersona(SteamUser.EPersonaState.Online);
     });
     client.on("error", function(error) {
         console.log(error);
     });
-    client.on("accountLimitations", function(limited, communityBanned, locked, canInviteFriends) {
-        var limitations = [];
-        if (limited) {
-            limitations.push("limited");
-        }
-        if (communityBanned) {
-            limitations.push("community banned");
-        }
-        if (locked) {
-            limitations.push("locked");
-        }
-        if (limitations.length === 0) {
-            console.log("Our account has no limitations");
-        } else {
-            console.log("Our account is " + limitations.join(", "));
-        }
-        if (canInviteFriends) {
-            console.log("Our account can invite friends");
-        }
+    client.on("accountInfo", function(name, country) {
+        mycountry = country;
     });
-    client.on("vacBans", function(numBans, appids) {
-        console.log("We have " + numBans + " VAC ban" + numberEnding(numBans.length));
-        if (appids.length > 0) {
-            console.log("We are VAC banned from app" + numberEnding(appids.length) + ": " + appids.join(", "));
-        }
-    });
-    client.on("webSession", function(sessionID, cookies) {
-        console.log("Got web session");
-        var community = new SteamCommunity();
-        community.setCookies(cookies);
-        community.httpRequestGet("https://steamcommunity.com/openid/login?openid.identity=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.claimed_id=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.ns=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0&openid.mode=checkid_setup&openid.realm=https%3A%2F%2Fsteamdb.info%2F&openid.return_to=https%3A%2F%2Fsteamdb.info%2Flogin%2F", {
-            followAllRedirects: true
-        }, function(error, response, data) {
-            if (error) {
-                console.log(error);
+    client.on("packageUpdate", function(packageid, data) {
+        console.log("Received PICS Update for Package " + packageid);
+        console.log(JSON.stringify(data, null, 4));
+        if (!ownedPackages.includes(packageid) &&
+            data.packageinfo.licensetype === 1 && // Single Purchase
+            data.packageinfo.status === 0 && // Available
+            (data.packageinfo.billingtype === 12 || data.packageinfo.billingtype === 0) && // NoCost or FreeOnDemand
+            (data.packageinfo.extended.purchaserestrictedcountries ? !data.packageinfo.extended.purchaserestrictedcountries.includes(mycountry) : true) &&
+            (data.packageinfo.ExpiryTime ? data.packageinfo.ExpiryTime > Math.round(Date.now() / 1000) : true) &&
+            (data.packageinfo.StartTime ? data.packageinfo.StartTime < Math.round(Date.now() / 1000) : true) &&
+            (data.packageinfo.DontGrantIfAppIDOwned ? !client.ownsApp(data.packageinfo.DontGrantIfAppIDOwned) : true) &&
+            (data.packageinfo.RequiredAppID ? client.ownsApp(data.packageinfo.RequiredAppID) : true)) {
+            if (fodRequested <= 50) {
+                requestFreeSub(packageid);
+            } else {
+                fodQueue.push(packageid);
             }
-            var url = $("#openidForm", data).attr("action");
-            var formdata = $("#openidForm", data).serializeObject();
-            community.httpRequestPost(url, {
-                followAllRedirects: true,
-                formData: formdata
-            }, steamdbLogin);
-        });
+        }
     });
     client.on("licenses", function(licenses) {
         console.log("Our account owns " + licenses.length + " license" + numberEnding(licenses.length));
-        var subs = [];
         licenses.forEach(function(license) {
-            subs.push(license.package_id);
+            if (!ownedPackages.includes(license.package_id)) {
+                ownedPackages.push(license.package_id);
+            }
         });
-        ownedSubs = subs;
-    });
-    client.on("appOwnershipCached", function() {
-        console.log("Cached app ownership");
     });
 }
 
-function steamdbLogin(error, response, data) {
-    console.log("Attempting to login to SteamDB")
-    if (error) {
-        console.log(error);
-        CloudScraper.request({
-            url: response.request.href,
-            method: "GET"
-        }, steamdbLogin);
-    } else {
-        var jar = request.jar();
-        response.headers["set-cookie"].forEach(function(cookiestr) {
-            var cookie = request.cookie(cookiestr);
-            jar.setCookie(cookie, "https://steamdb.info/");
-        });
-        CloudScraper.request({
-            //url: "https://steamdb.info/search/?a=app_keynames&type=-1&keyname=243&operator=3&keyvalue=1",
-            url: "https://steamdb.info/search/?a=sub_keynames&keyname=1&operator=3&keyvalue=12",
-            method: "GET",
-            jar: jar
-        }, function(error, response, data) {
-            if (error) {
-                console.log(error);
+function requestFreeSub(packageid) {
+    console.log("Attempting to request package id " + packageid);
+    fodRequested++;
+    client.requestFreeLicense(packageid, function(error, grantedPackages, grantedAppIDs) {
+        if (error) {
+            console.log(error)
+        } else {
+            if (grantedPackages.length === 0) {
+                console.log("No new packages were granted to our account");
             } else {
-                var freeSubs = [];
-                $("#table-sortable tr a", data).each(function() {
-                    freeSubs.push(parseInt($(this).text().trim()));
-                });
-                var ownedSubsPromise = setInterval(function() {
-                        if (ownedSubs.length > 0) {
-                            clearInterval(ownedSubsPromise);
-                            var unownedFreeSubs = $(freeSubs).not(ownedSubs).get().reverse();
-                            console.log("Found " + freeSubs.length + " free sub" + numberEnding(freeSubs.length) + " of which " + unownedFreeSubs.length + " are not owned by us yet");
-                            requestFreeSubs(unownedFreeSubs);
-                        }
-                    }, 10);
+                console.log(grantedPackages.length + " New package" + numberEnding(grantedPackages.length) + " (" + grantedPackages.join() + ") were successfully granted to our account");
+                if (!ownedPackages.includes(packageid) && grantedPackages.includes(packageid)) {
+                    ownedPackages.push(packageid);
+                }
             }
-        });
-    }
-}
-
-function requestFreeSubs(unownedFreeSubs) {
-    if (unownedFreeSubs.length > 0) {
-        var subsToAdd = unownedFreeSubs.splice(0, config.max_subs);
-        console.log("Attempting to request " + subsToAdd.length + " subs (" + subsToAdd.join() + ")");
-        client.requestFreeLicense(subsToAdd, function(error, grantedPackages, grantedAppIDs) {
-            if (error) {
-                console.log(error)
+            if (grantedAppIDs.length === 0) {
+                console.log("No new apps were granted to our account");
             } else {
-                if (grantedPackages.length === 0) {
-                    console.log("No new packages were granted to our account");
-                } else {
-                    console.log(grantedPackages.length + " New package" + numberEnding(grantedPackages.length) + " (" + grantedPackages.join() + ") were successfully granted to our account");
-                }
-                if (grantedAppIDs.length === 0) {
-                    console.log("No new apps were granted to our account");
-                } else {
-                    console.log(grantedAppIDs.length + " New app" + numberEnding(grantedAppIDs.length) + " (" + grantedAppIDs.join() + ") were successfully granted to our account");
-                }
-                console.log("Waiting " + millisecondsToStr(config.delay) + " for a new attempt");
-                setTimeout(function() {
-                    requestFreeSubs(unownedFreeSubs)
-                }, config.delay);
+                console.log(grantedAppIDs.length + " New app" + numberEnding(grantedAppIDs.length) + " (" + grantedAppIDs.join() + ") were successfully granted to our account");
             }
-        });
-    } else {
-        client.relog();
-    }
+        }
+    });
 }
 
 function millisecondsToStr(milliseconds) {
     var temp = Math.floor(milliseconds / 1000);
     var years = Math.floor(temp / 31536000);
     if (years) {
-        return years + ' year' + numberEnding(years);
+        return years + " year" + numberEnding(years);
     }
     var days = Math.floor((temp %= 31536000) / 86400);
     if (days) {
-        return days + ' day' + numberEnding(days);
+        return days + " day" + numberEnding(days);
     }
     var hours = Math.floor((temp %= 86400) / 3600);
     if (hours) {
-        return hours + ' hour' + numberEnding(hours);
+        return hours + " hour" + numberEnding(hours);
     }
     var minutes = Math.floor((temp %= 3600) / 60);
     if (minutes) {
-        return minutes + ' minute' + numberEnding(minutes);
+        return minutes + " minute" + numberEnding(minutes);
     }
     var seconds = temp % 60;
     if (seconds) {
-        return seconds + ' second' + numberEnding(seconds);
+        return seconds + " second" + numberEnding(seconds);
     }
-    return 'less than a second';
+    return "less than a second";
 }
 
 function numberEnding(number) {
-    return (number > 1) ? 's' : '';
+    return (number > 1) ? "s" : "";
 }
-
-$.fn.serializeObject = function() {
-    var o = {};
-    var a = this.serializeArray();
-    $.each(a, function() {
-        if (o[this.name] !== undefined) {
-            if (!o[this.name].push) {
-                o[this.name] = [o[this.name]];
-            }
-            o[this.name].push(this.value || "");
-        } else {
-            o[this.name] = this.value || "";
-        }
-    });
-    return o;
-};
